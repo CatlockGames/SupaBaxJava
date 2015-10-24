7046c143d3efa1c402d2a71772e0af94b2595c2a
Aaron Shappell <crumster88@gmail.com>
2015-10-24 13:42:34 +0300

    Add initial small enemy class

---
+++ core/src/crate/SmallEnemy.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package crate;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * @author Aaron
+ *
+ */
+public class SmallEnemy extends Entity {
+
+	/**
+	 * 
+	 */
+	public SmallEnemy() {
+	}
+
+	@Override
+	public void update(float delta) {
+	}
+
+	@Override
+	public void render(SpriteBatch batch, float delta) {
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+}
